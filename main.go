147075eb937c3a55b965808ecd16e92943ae79b8
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-01-08 11:41:39 -0300

    feat: Implements dinamic products slice

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Products struct {
+	Nome        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Products{
+		{Nome: "T-Shirt", Description: "Blue", Price: 50, Quantity: 5},
+		{"Shoes", "Red", 100, 2},
+		{"New Product", "Promotion", 57, 1},
+	}
+
+	templates.ExecuteTemplate(w, "Index", products)
 }
